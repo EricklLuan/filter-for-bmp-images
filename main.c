64e88e64b5143e0620b83b756aff7b0d90dddded
Erick Luan <flinprice1308ft@gmail.com>
2022-07-24 13:36:03 -0300

    Changing Image Colours

--- main.c
+++ main.c
@@ -18,37 +18,75 @@ typedef struct {
     uint16_t bits;              // The number of bits per pixel
     uint32_t compression;       // The compression type
     uint32_t img_size;          // The image size in bytes
-    int32_t x_resolution;       // The x pixel per meter
-    int32_t y_resolution;       // The y pixel per meter
+    int32_t x_pixelpermeter;    // The x pixel per meter
+    int32_t y_pixelpermeter;    // The y pixel per meter
     uint32_t n_colours;         // Numbers of colours
     uint32_t important_colours; // Important colours
 } __attribute__ ((packed)) BMPINFOHEADER;
 
-int main(int argc, char** argv) {
-    FILE *image;
-    BMPFILEHEADER header;
-    BMPINFOHEADER info_header;
+typedef struct {
+    uint32_t R;
+    uint32_t G;
+    uint32_t B;
+    uint32_t reserved; 
+} PIXEL;
 
-    image = fopen("assets/campo.bmp", "rb");
-    if (!image) {
-        printf("Could not open the file %s", "assets/campo.bmp");
+int main(int argc, char** argv) {
+    
+    // Open the bmp image
+    FILE *image = fopen(argv[1], "rb");
+    if (image == NULL) {
+        printf("Could not open the file %s", argv[1]);
         fclose(image);
         return -1;
     }
 
+    // Give to the header var the image data
+    BMPFILEHEADER header;
     fread(&header, sizeof(BMPFILEHEADER), 1, image);
     if (header.type != 0x4D42) {
-        printf("The file %s is not a valid BMP.\n", "assets/campo.bmp");
+        printf("The file %s is not a valid BMP.\n", argv[1]);
         fclose(image);
         return -1;
     }
 
+    // Give to the info header var the image data
+    BMPINFOHEADER info_header;
     fread(&info_header, sizeof(BMPINFOHEADER), 1, image);
     if (info_header.bits != 24) {
-        printf("The file %s is not a valid BMP.\n", "assets/campo.bmp");
+        printf("The file %s is not a valid BMP.\n", argv[1]);
         fclose(image);
         return -1;
     }
 
+    // Open's or create the image that contains the updated image
+    FILE *result = fopen(argv[3], "wb");
+    if (result == NULL) {
+        printf("Could not create or update the file %s", argv[3]);
+        fclose(result);
+        fclose(image);
+        return -1;
+    }
+    
+    // Copy the image data to the result data
+    fwrite(&header, sizeof(BMPFILEHEADER), 1, result);
+    fwrite(&info_header, sizeof(BMPINFOHEADER), 1, result);
+    
+    // Goes through all the pixels in the image and replace their colors
+    PIXEL color;
+    for (int i = 0; i < info_header.height; i++) {
+        for (int j = 0; j < info_header.width; j++) {
+            // Get the pixel data
+            fread(&color, sizeof(PIXEL), 1, image);
+            color.R = color.B;
+            color.G = color.B;
+            color.B = color.R;
+            // Give the updated pixel data to the result image
+            fwrite(&color, sizeof(PIXEL), 1, result);
+        }
+    }
+    
+    fclose(image);
+    fclose(result);
     return 0;
 }
